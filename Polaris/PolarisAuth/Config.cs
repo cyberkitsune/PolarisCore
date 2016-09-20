3a1e7a1c3256c3f329b341415c1a9783797de5e8
Kyle Belanger <kyle873@gmail.com>
2016-09-20 01:42:18 -0400

    Added NewtonSoft.Json as a dependency to the projects, added PolarisAuth JSON configuration, handles #18 and #20

---
+++ Polaris/PolarisAuth/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace Polaris.Auth
+{
+    public class Config
+    {
+		public string ClientVersion { get; } = "4.0402.1";
+
+		public string BindIP { get; } = "127.0.0.1";
+		public uint Port { get; } = 12300;
+
+		public string RSAPublicKey { get; } = "publicKey.blob"; // File Path
+		public string RSAPrivateKey { get; } = "privateKey.blob"; // File Path
+
+		public string DatabaseHost { get; } = "127.0.0.1";
+		public string DatabaseUsername { get; } = "Polaris";
+		public string DatabasePassword { get; } = "Polaris";
+		public string DatabaseName { get; } = "Polaris";
+
+		public static void Create(string filename)
+		{
+			File.WriteAllText(filename, JsonConvert.SerializeObject(new Config(), Formatting.Indented));
+		}
+
+		public static Config Load(string filename)
+		{
+			return JsonConvert.DeserializeObject<Config>(File.ReadAllText(filename));
+		}
+    }
+}
--- Polaris/PolarisAuth/Program.cs
+++ Polaris/PolarisAuth/Program.cs
@@ -1,13 +1,23 @@
 using System;
+using System.IO;
 
 namespace Polaris.Auth
 {
 	public class Program
     {
+		static Config config;
+
         public static void Main(string[] args)
         {
+			if (!File.Exists("PolarisAuth.json"))
+				Config.Create("PolarisAuth.json");
+
+			config = Config.Load("PolarisAuth.json");
+
             Console.WriteLine("Hello world!");
-            Console.ReadLine();
+			Console.WriteLine($"Client Version: {config.ClientVersion}");
+
+			Console.ReadLine();
         }
     }
 }
